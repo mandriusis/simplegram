d5adb60db0529f5c2c49bd19e9034bae4f6dd653
Matt Andriusis <mandriusis@gmail.com>
2020-07-17 18:45:51 -0500

    Basic navbar stubbed in

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-const webpack = require("webpack")
-
-environment.plugins.append(
-  "Provide",
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery",
-    Rails: "@rails/uks"
-  })
-)
-
 # Initialize the Rails application.
 Rails.application.initialize!
