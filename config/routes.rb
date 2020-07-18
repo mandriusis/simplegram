da94d740d4bd6a1857992d27fdbb0b46b1dcce44
Matt Andriusis <mandriusis@gmail.com>
2020-07-18 11:13:07 -0500

    Added basic pagination for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts do 
     resource :likes, only: [:create, :destroy], module: :posts
-    resources :comments, only: [:create, :new, :destroy], module: :posts
+    resources :comments, only: [:show, :create, :new, :destroy], module: :posts
   end
   resources :users, only: [:show], param: :username, path: '' do
     resources :posts, only: [:show], module: :users
